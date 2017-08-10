3f9c6a1b0851c90a406280e88ba3324939b8035d
Pulasthi Bandara <pulasthi@Pulasthis-Air.fritz.box>
2017-08-11 08:52:53 +1200

    added a socket exchange

--- src/WebSocketExchange.ts
+++ src/WebSocketExchange.ts
@@ -22,22 +22,33 @@ export default class WebSocketExchange {
   }
 
   handleConnection(ws: WebSocket, req: IncomingMessage) {
-    ws.on('open', () => {
-      ws.send('connection successful!');
-    });
+    ws.on('open', () => ws.send('connection successful!'));
 
-    ws.on('message', (data) => {
-      try {
-        const post = <Post>JSON.parse(data.toString());
-        // console.log(this.postRepository);
-        this.postRepository.create(post).then((post) => {
-          // ws.send(JSON.stringify(post.ops));
-          this.server.clients.forEach(c => c.send(JSON.stringify(post.ops)));
-        });
-      } catch (err) {
-        console.error(err);
-        ws.send('error');
+    ws.on('message', data => this.messageTransformer(ws, data));
+
+    ws.on(
+      'sock_join',
+      ((data: any) => this.broadcastToOthers(ws, JSON.stringify(data))) as any
+    );
+  }
+
+  messageTransformer(ws: WebSocket, rawData: WebSocket.Data) {
+    try {
+      const data = JSON.parse(rawData.toString());
+      if (data.action && data.payload) {
+        ws.emit(`sock_${data.action}`, data.payload);
+        return;
       }
-    });
+
+      throw Error('invalid payload');
+    } catch (err) {
+      console.error(err);
+      ws.emit(err.message);
+    }
+  }
+
+  broadcastToOthers (ws: WebSocket, data: WebSocket.Data) {
+    this.server.clients
+      .forEach(c => ws !== c ? c.send(data) : null);
   }
 }
